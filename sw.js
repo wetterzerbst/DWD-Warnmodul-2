c6cc7ce3d4a499316262581ac44d998f2d383ac8
Wetterstation Zerbst-Dorfstätte <wetter.zerbst@gmail.com>
2024-11-06 00:32:10 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ self.addEventListener('notificationclick', function(event) {
   }).then(function(clientList) {  //console.log(event)
     for (var i = 0; i < clientList.length; i++) {
       var client = clientList[i];
-      if (client.url == 'https://dj0001.github.io/DWD-Warnmodul-2/' && 'focus' in client)
+      if (client.url == 'https://wetterzerbst.github.io/DWD-Warnmodul-2/' && 'focus' in client)
         return client.focus();
     }
     if (clients.openWindow)
